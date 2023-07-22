b364eab408f4fad447a380b997e7f535fcf1dd7e
Jabulani-N <jabu.94@gmail.com>
2023-07-21 17:14:08 -0700

    task0: style improvements

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-""" remember the shebang
-Write a Python script that,
-using this REST API,
-for a given employee ID (input argument),
-returns information about their todo list progress
-"""
+"""returns information about emplopyee todo list progress"""
 
 from flask import Flask
-import requests, sys
+import requests
+import sys
 app = Flask(__name__)
 
+
 def gather_data():
-    """ gets users and todo lists from api
+    """gets users and todo lists from api
     user id is the second argument
     you can
     for items in todos
@@ -22,11 +19,11 @@ def gather_data():
 
     id = sys.argv[1]  # user gives id number in request.
     # argv[0] is funciton name
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'\
-                              .format(id)).json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(id)).json()
     # This is one user's data. it's user with the requested id
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos',\
-                          params={"userId": id}).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos',
+                         params={"userId": id}).json()
     # get requests where the conditions in params are met
     # traveling to the url, todos looks like an array [] of dictionaries {}
     username = user.get("name")
@@ -36,12 +33,14 @@ def gather_data():
 
     for task in todos:
         tasks_total += 1
-        if task['completed'] == True:
+        if task['completed'] is True:
             tasks_done += 1
             tasks_titles.append(task['title'])
-    print('Employee ' + username + ' is done with tasks(' + str(tasks_done) + '/' + str(tasks_total) + ')')
+    print('Employee ' + username + ' is done with tasks(' +
+          str(tasks_done) + '/' + str(tasks_total) + ')')
     for taskname in tasks_titles:
         print('\t ' + taskname)
+
 
 if __name__ == '__main__':
     gather_data()
